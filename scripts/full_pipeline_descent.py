d763b9a135838d8b958643f31d7605db4d0711f0
Julia310 <JPeters92@protonmail.com>
2023-03-18 18:59:17 +0100

    seed independent gen

--- scripts/full_pipeline_descent.py
+++ scripts/full_pipeline_descent.py
@@ -1,109 +1,48 @@
 from ldm.stable_diffusion import StableDiffusion
-import torch
-from aesthetic_predictor.simple_inference import AestheticPredictor
-from torchvision.transforms import CenterCrop, Resize, Normalize, InterpolationMode
 from optimizer.adam_on_lion import AdamOnLion
-from torch.nn import functional as F
+import torch
 
 seed = 61582
+seed2 = 615845
+seed3 = 82641
 dim = 512
 
 device = 'cuda'
 
 ldm = StableDiffusion(device=device)
-aesthetic_predictor = AestheticPredictor(device=device)
 
-prompt1 = 'a beautiful painting of a peaceful lake in the Land of the Dreams, full of grass, sunset, red horizon, ' \
+prompt = 'a beautiful painting of a peaceful lake in the Land of the Dreams, full of grass, sunset, red horizon, ' \
          'starry-night!!!!!!!!!!!!!!!!!!!!,  Greg Rutkowski, Moebius, Mohrbacher, peaceful, colorful'
 
 
-#prompt2 = "ugly meme, funniest thing ever"
-#prompt3 = "a dad angry at missing his flight from prague to nyc, the dad is drunk "
-
-#prompt4 = "Cute small humanoid bat sitting in a movie theater eating popcorn watching a movie ,unreal engine, cozy " \
-#         "indoor lighting, artstation, detailed, digital painting,cinematic,character design by mark ryden and pixar " \
-#         "and hayao miyazaki, unreal 5, daz, hyperrealistic, octane render"
-
-#prompts = [prompt1, prompt2, prompt3]
-
-
-def compute_blurriness(image):
-    # Convert the image to grayscale
-    gray_image = 0.2989 * image[:, 0, :, :] + 0.5870 * image[:, 1, :, :] + 0.1140 * image[:, 2, :, :]
-
-    # Compute the Laplacian filter
-    laplacian_filter = torch.tensor([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
-    laplacian_filter = laplacian_filter.to(gray_image.device).to(gray_image.dtype)
-
-    # Apply the Laplacian filter to the grayscale image
-    filtered_image = F.conv2d(gray_image.unsqueeze(0), laplacian_filter.unsqueeze(0).unsqueeze(0))
-
-    # Compute the variance of the Laplacian filter response
-    variance = torch.var(filtered_image)
-
-    return variance
-
-
-def compute_metric(image):
-    # Convert the image to grayscale
-    gray_image = 0.2989 * image[:, 0, :, :] + 0.5870 * image[:, 1, :, :] + 0.1140 * image[:, 2, :, :]
-
-    # Compute the Laplacian filter
-    #laplacian_filter = torch.tensor([[1.0/9, 1.0/9, 1.0/9], [1.0/9, 1.0/9, 1.0/9], [1.0/9, 1.0/9, 1.0/9]])
-    laplacian_filter = torch.tensor([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
-    laplacian_filter = laplacian_filter.to(gray_image.device).to(gray_image.dtype)
-
-    # Apply the Laplacian filter to the grayscale image
-    filtered_image = F.conv2d(gray_image.unsqueeze(0), laplacian_filter.unsqueeze(0).unsqueeze(0))
-
-    # Compute the variance of the Laplacian filter response
-    variance = torch.var(filtered_image)
-
-    return variance
-
-
-def grayscale(image):
-    score = - torch.mean(torch.std(image, dim=1)) * 100
-    return score
-
-
-def compute_colorfulness(image):
-    # Compute the mean and standard deviation of the RGB channels
-    mean = torch.mean(image, dim=(2, 3))
-    std = torch.std(image, dim=(2, 3))
-
-    # Compute the colorfulness metric
-    colorfulness = torch.sum(std) / (3 * torch.mean(mean))
-
-    return colorfulness
-
-
-def preprocess(rgb):
-    rgb = Resize(size=224, interpolation=InterpolationMode.BICUBIC, max_size=None, antialias=None)(rgb)
-    rgb = CenterCrop(size=(224, 224))(rgb)
-    rgb = Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))(rgb)
-    return rgb
+def get_shifted_embedding(text_embedding, default_std, default_mean):
+    shifted_text_embedding = text_embedding / (torch.std(text_embedding)) * default_std
+    shifted_text_embedding = shifted_text_embedding - torch.mean(shifted_text_embedding) + default_mean
+    return shifted_text_embedding
 
 
 class GradientDescent(torch.nn.Module):
-    def __init__(self, text_embedding):
+    def __init__(self, text_embedding, target_latents, comb_init_latents):
         super().__init__()
         self.text_embedding = torch.nn.Parameter(text_embedding)
         self.text_embedding.requires_grad = True
         self.latents = None
+        self.target_latents = target_latents
+        ldm.initial_latents = comb_init_latents
+        with torch.no_grad():
+            self.default_mean = torch.mean(text_embedding)
+            self.default_std = torch.std(text_embedding)
 
-    def forward(self, g=7.5, steps=70):
-        latents = ldm.embedding_2_img('', self.text_embedding, dim=dim, seed=seed, return_pil=False, g=g, steps=steps)
-        self.latents = latents
-        image = ldm.latents_to_image(latents, return_pil=False)
+    def forward(self):
+        # print(f'min: {torch.min(shifted_text_embedding)}, max: {torch.max(torch.max(shifted_text_embedding))}')
+        latents = ldm.embedding_2_img('', self.text_embedding, save_img=False, dim=dim, return_pil=False,
+                                      return_latents=True, keep_init_latents=True)
 
-        image = preprocess(image)
-        image_embedding = aesthetic_predictor.clip.encode_image(image).float()
-        image_embedding = aesthetic_predictor.get_features(image_embedding, image_input=False)
-        score = aesthetic_predictor.mlp(image_embedding).squeeze()
-        print(score)
+        latents = latents.flatten(start_dim=1, end_dim=-1)
 
-        return score
+        distance = torch.dist(self.target_latents.to(torch.float64), latents.to(torch.float64))
+        print(distance)
+        return distance
 
     def get_optimizer(self, eta, optim='SGD'):
         if optim == 'SGD':
@@ -117,52 +56,51 @@ class GradientDescent(torch.nn.Module):
             return torch.optim.Adam(
                 self.parameters(),
                 lr=eta
-                #eps=0.00000001
+                # eps=0.00000001
             )
         else:
             return AdamOnLion(
-                params=gradient_descent.parameters(),
+                params=self.parameters(),
                 lr=eta,
                 eps=0.001,
             )
 
 
-
 if __name__ == '__main__':
-    prompt = prompt1
-    gradient_descent = GradientDescent(ldm.get_embedding([prompt])[0])
+    embedding = ldm.get_embedding([prompt])[0]
 
-    eta = 0.01
-    num_images = 1000
+    with torch.no_grad():
+        target_latents = ldm.embedding_2_img('', embedding, save_img=False, dim=dim, seed=seed, return_pil=False,
+                                             return_latents=True, keep_init_latents=False)
 
-    optimizer = gradient_descent.get_optimizer(eta, 'AdamOnLion')
+    target_init_latents = torch.clone(ldm.initial_latents)
+    ldm.embedding_2_img('', embedding, dim=dim, seed=seed2, return_latents=True, keep_init_latents=False)
 
-    score = 0
-    cnt = 0
-    #for i in range(num_images):
-    while score < 6.93 and cnt < 350 or score < 7.1:
+    combined_init_latents = ldm.combine_embeddings(target_init_latents, ldm.initial_latents, 0.02)
+
+    target_latents = target_latents.flatten(start_dim=1, end_dim=-1)
+
+    gd = GradientDescent(embedding, target_latents, combined_init_latents)
+
+    eta = 0.001
+    num_images = 300
+
+    optimizer = gd.get_optimizer(eta, 'AdamOnLion')
+
+    for i in range(num_images):
         optimizer.zero_grad()
-        score = gradient_descent.forward(steps=70)
-        loss = -score
-        loss.backward()
+        score = gd.forward()
+        loss = score
+        loss.backward(retain_graph=True)
         optimizer.step()
-        cnt += 1
 
 
+        # print(f'std: {torch.std(gd.text_embedding)}, mean: {torch.mean(gd.text_embedding)}')
+        pil_img = ldm.embedding_2_img('', gd.text_embedding,
+                                      return_pil=True, save_img=False,
+                                      return_latents=False, keep_init_latents=True)
+        pil_img.save(f'output/{i}_{prompt[0:25]}_{score.item()}.jpg')
 
-    embedding1 =  ldm.get_embedding([prompt])[0]
-    pil_image = ldm.latents_to_image(gradient_descent.latents)[0]
-    pil_image.save(f'output/50_{prompt[0:45]}_{score.item()}.jpg')
-
-    pil_image = ldm.embedding_2_img('', embedding1, dim=dim, seed=seed, return_pil=True, steps=70, save_img=False)
-    pil_image.save(f'output/0_{prompt[0:45]}_{score.item()}.jpg')
-
-    embedding2 = gradient_descent.text_embedding
-
-    for i in range(49):
-        alpha = (i+1) * 0.02
-        print(alpha)
-
-        combined_embedding = ldm.combine_embeddings(embedding1, embedding2, alpha)
-        pil_image = ldm.embedding_2_img('', combined_embedding, dim=dim, seed=seed, return_pil=True, steps=70, save_img=False)
-        pil_image.save(f'output/{i + 1}_{prompt}.jpg')
+        # if (i + 1) % 75 == 0:
+        #    gd.text_embedding = torch.nn.Parameter(get_shifted_embedding(gd.text_embedding, gd.default_std, gd.default_mean))
+        #    gd.text_embedding.requires_grad = True
